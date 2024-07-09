6dcb78943f07731deb25c9f470a69aba3054a6aa
stanislav.b@gowombat.team <stanislav.b@gowombat.team>
2024-07-09 15:32:35 +0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
+import bodyParser from 'body-parser'
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
-const port = process.env.PORT || 3000
 
-const jsonBodyMiddleware = express.json()
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+const jsonBodyMiddleware = bodyParser.json()
 app.use(jsonBodyMiddleware)
+
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello world')
